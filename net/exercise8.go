8e90de61bd7e6d4eb6d03954277c8675a64ac3c5
Nishant Sharma <sushank123456@gmail.com>
2023-08-22 22:38:38 +0530

    completing url fetching exercise

--- net/exercise8.go
+++ net/exercise8.go
@@ -1,4 +1,3 @@
-// Fetch prints the content found at a URL.
 package main
 
 import (
@@ -6,10 +5,16 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
-func main() {
-	for _, url := range os.Args[1:] {
+
+func main(){
+	for _, url := range os.Args[1:]{
+		if !strings.HasPrefix(url,"http://"){
+			url = "http://" +  url
+		}
+
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
@@ -27,3 +32,5 @@ func main() {
 		fmt.Printf("%v", bytes)
 	}
 }
+
+
--- net/exercise9.go
+++ net/exercise9.go
@@ -1,4 +1,3 @@
-// Fetch prints the content found at a URL.
 package main
 
 import (
@@ -6,10 +5,16 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
-func main() {
-	for _, url := range os.Args[1:] {
+
+func main(){
+	for _, url := range os.Args[1:]{
+		if !strings.HasPrefix(url,"http://"){
+			url = "http://" +  url
+		}
+
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
@@ -24,6 +29,9 @@ func main() {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
 		}
+		fmt.Printf("Status Code : %v", resp.StatusCode)
 		fmt.Printf("%v", bytes)
 	}
 }
+
+
